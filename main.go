ffacaf893a9b81abb589b0882dfd056c85e0e5a2
soggiest <nicholas.lane@coreos.com>
2017-05-04 09:31:10 -0700

    testing anything

--- main.go
+++ main.go
@@ -9,16 +9,16 @@ import (
 
 func main() {
     fmt.Printf("TEST1")
-    client, err := k8s.NewInClusterClient()
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    nodes, err := client.CoreV1().ListNodes(context.Background())
-    if err != nil {
-        log.Fatal(err)
-    }
-    for _, node := range nodes.Items {
-        fmt.Printf("name=%q schedulable=%t\n", *node.Metadata.Name, !*node.Spec.Unschedulable)
-    }
+    #    client, err := k8s.NewInClusterClient()
+    #    if err != nil {
+    #        log.Fatal(err)
+    #    }
+    #
+    #    nodes, err := client.CoreV1().ListNodes(context.Background())
+    #    if err != nil {
+    #        log.Fatal(err)
+    #    }
+    #    for _, node := range nodes.Items {
+    #        fmt.Printf("name=%q schedulable=%t\n", *node.Metadata.Name, !*node.Spec.Unschedulable)
+    #    }
 }
