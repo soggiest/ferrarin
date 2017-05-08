cba616bf5bc6b25fe7c649e9c76dec15f02976d0
soggiest <nicholas.lane@coreos.com>
2017-05-08 11:11:54 -0700

    Config stuff

--- main.go
+++ main.go
@@ -14,17 +14,17 @@ func main() {
 //fmt.Printf("TEST1")
   config, err := rest.InClusterConfig()
   if err != nil {
-    panic(err.Error())
+//    panic(err.Error())
   }
   fmt.Printf(config.Host)
   clientset, err := kubernetes.NewForConfig(config)
   if err != nil {
-    panic(err.Error())
+//    panic(err.Error())
   }
   for {
     pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
     if err != nil {
-      panic(err.Error())
+//      panic(err.Error())
     }
     fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
 //		time.Sleep(10 * time.Second)
