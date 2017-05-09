8079ea231a88e24cf21526289bcfeebadc439c02
soggiest <nicholas.lane@coreos.com>
2017-05-08 19:59:43 -0700

    switched back to Eric's k8s client

--- main.go
+++ main.go
@@ -1,44 +1,46 @@
 package main
 
 import (
-//  "context"
+  "context"
     "fmt"
-//    "log"
-//    "github.com/ericchiang/k8s"
-    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-    "k8s.io/client-go/kubernetes"
-    "k8s.io/client-go/rest"
+//    "time"
+    "log"
+    "github.com/ericchiang/k8s"
+//    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+//    "k8s.io/client-go/kubernetes"
+//    "k8s.io/client-go/rest"
 )
 
 func main() {
   fmt.Printf("TEST1")
-  config, err := rest.InClusterConfig()
-  if err != nil {
-    panic(err.Error())
-  }
-  fmt.Printf(config.Host)
-  clientset, err := kubernetes.NewForConfig(config)
-  if err != nil {
-    panic(err.Error())
-  }
-  for {
-    pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
-    if err != nil {
-      panic(err.Error())
-    }
-    fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
-		time.Sleep(10 * time.Second)
-  }
+//  config, err := rest.InClusterConfig()
+//  if err != nil {
+//    panic(err.Error())
+//  }
+//  fmt.Printf(config.Host)
+//  clientset, err := kubernetes.NewForConfig(config)
+//  if err != nil {
+//    panic(err.Error())
+//  }
+//  for {
+//    pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
+//    if err != nil {
+//      panic(err.Error())
+//    }
+//    fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+//		time.Sleep(10 * time.Second)
+//  }
 
-//    client, err := k8s.NewInClusterClient()
-//    if err != nil {
-//      log.Fatal(err)
-//    }
-//    pods, err := client.CoreV1().ListPods(ctx, client.Namespace)
-//    if err != nil {
-//      log.Fatal(err)
-//    }
-//    for _, pod := range pods.Items {
-//      fmt.Printf("%q", *pod.Metadata.Name)
-//    }
+    ctx := context.Background()
+    client, err := k8s.NewInClusterClient()
+    if err != nil {
+      log.Fatal(err)
+    }
+    pods, err := client.CoreV1().ListPods(ctx, client.Namespace)
+    if err != nil {
+      log.Fatal(err)
+    }
+    for _, pod := range pods.Items {
+      fmt.Printf("%q", *pod.Metadata.Name)
+    }
 }
