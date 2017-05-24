3613ec43dc150ea696062221df93b072fc4d212d
soggiest <nicholas.lane@coreos.com>
2017-05-24 10:08:17 -0700

    Initial Release Test

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ import (
 	//   "k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	//   "k8s.io/client-go/pkg/api/v1"
-	"github.com/soggiest/ferrarin/tests/createpod"
+	"github.com/soggiest/ferrarin/createpod"
+	"github.com/soggiest/ferrarin/networktest"
 	//v1 "k8s.io/client-go/pkg/api/v1"
 )
 
@@ -21,24 +22,34 @@ var (
 	kubeconfig = flag.String("kubeconfig", "./config", "absolute path to the kubeconfig file")
 )
 
+func cleanup(client *kubernetes.Clientset, createPod string) {
+	if len(createPod) > 0 {
+		fmt.Println("Cleaning up test-pods-server DaemonSet")
+		createpod.Cleanup(client)
+	}
+}
+
 func main() {
 	//TODO: GET RID OF THE OUT OF CLUSTER CONFIG WHEN PUSHING INTO THE CLUSTER
 	flag.Parse()
+
+	createPod := os.Getenv("CREATE_POD")
+	networkTest := os.Getenv("NETWORK_TEST")
+	prometheusConnect := os.Getenv("PROMETHEUS_CONNECT")
 	// uses the current context in kubeconfig
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	//	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	//	if err != nil {
+	panic(err.Error())
+	//	}
+	config, err := rest.InClusterConfig()
 	if err != nil {
 		panic(err.Error())
 	}
-	//  config, err := rest.InClusterConfig()
-	//  if err != nil {
-	//    panic(err.Error())
-	//  }
 	client, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	createPod := os.Getenv("CREATE_POD")
 	if len(createPod) != 0 {
 		if createPod == "true" {
 			//fmt.Println(len(create_pod))
@@ -54,4 +65,34 @@ func main() {
 		glog.V(2).Infof("CREATE_POD environment variable missing, skipping test")
 	}
 
+	if len(networkTest) != 0 {
+		if networkTest == "supertrue" {
+			networktest.NetworkTest(client)
+		} else {
+			fmt.Println("NETWORK_TEST environment variable set to false, skipping test")
+			glog.Info("NETWORK_TEST environment variable set to false, skipping test")
+
+		}
+
+	} else {
+		fmt.Println("NETWORK_TEST environment variable missing, skipping test")
+		glog.V(2).Infof("NETWORK_TEST environment variable missing, skipping test")
+
+	}
+
+	if len(prometheusConnect) != 0 {
+		if prometheusConnect == "true" {
+			createpod.ConnectPrometheus(config)
+		} else {
+			fmt.Println("PROMETHEUS_CONNECT environment variable set to false, not adding ")
+		}
+
+	} else {
+		fmt.Println("PROMETHEUS_CONNECT evironment variable missing, not adding Test pods to Prometheus")
+
+	}
+
+	if len(os.Getenv("CLEANUP")) > 0 {
+		defer cleanup(client, createPod)
+	}
 }
